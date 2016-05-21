f5aa7df260c3f6f20b0f93a974a7c96588d11521
Lukasz <l.j.tarnowski@gmail.com>
2016-05-21 14:57:32 +0000

    wyszukiwanie jest i dziala

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
+	
+		def self.search(search)
+			    where("title LIKE ? or body LIKE ?", "%#{search}%", "%#{search}%") 
+		end
 end
